dfd7ab3dfb5df1c8c39f95eec7872d446468f61f
Nemanja Divnic <ndivnic@tenstorrent.com>
2026-02-10 13:00:35 +0000

    Add LLK_ASSERT for tile index in dest register.

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_pack_api.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_pack_api.h
@@ -148,9 +148,11 @@ inline void llk_pack(std::uint32_t tile_index, std::uint32_t output, std::uint32
     std::uint8_t output_id = get_output_id(output);
 
     static_assert((!(untilize && out_of_order_output)) && "untilize out of order packing is not supported!");
+    LLK_ASSERT(
+        (tile_index < get_dest_max_tiles<DST_SYNC_MODE, DST_ACCUM_MODE, DstTileShape::Tile32x32>()),
+        "Tile index exceeds maximum allowed for the given tile shape and accumulation mode.");
 
     std::uint32_t pack_tile_addr = get_output_tile_address<out_of_order_output, untilize>(output_id, output_tile_index);
-
     _llk_pack_<DST_SYNC_MODE, is_fp32_dest_acc_en, untilize>(tile_index, pack_tile_addr);
 }
 
@@ -214,9 +216,12 @@ inline void llk_matmul_pack(
     static_assert((!(untilize && out_of_order_output)) && "untilize out of order packing is not supported!");
 
     for (uint32_t tile_index = start_tile_index; tile_index < start_tile_index + ntiles; tile_index++) {
+        LLK_ASSERT(
+            (tile_index < get_dest_max_tiles<DST_SYNC_MODE, DST_ACCUM_MODE, DstTileShape::Tile32x32>()),
+            "Tile index exceeds maximum allowed for the given tile shape and accumulation mode.");
+
         std::uint32_t pack_tile_addr =
             get_output_tile_address<out_of_order_output, untilize>(output_id, output_tile_index);
-
         _llk_pack_<DST_SYNC_MODE, is_fp32_dest_acc_en, untilize>(tile_index, pack_tile_addr);
     }
 }
@@ -251,6 +256,10 @@ inline void llk_pack_rows_init(const std::uint32_t num_rows) { _llk_pack_rows_in
  */
 inline void llk_pack_rows(
     const std::uint32_t dst_index, const std::uint32_t output, const std::uint32_t output_index = 0) {
+    LLK_ASSERT(
+        (dst_index < get_dest_max_tiles<DST_SYNC_MODE, DST_ACCUM_MODE, DstTileShape::Tile32x32>()),
+        "Dst tile exceeds maximum allowed for the given tile shape and accumulation mode.");
+
     const std::uint8_t output_id = get_output_id(output);
     const std::uint32_t pack_addr = get_output_tile_address<true, false>(output_id, output_index);
     _llk_pack_rows_(dst_index, pack_addr);
@@ -295,10 +304,12 @@ inline void llk_pack_fast_tilize_block(
     const std::uint32_t output_tile_index,
     const std::uint32_t unit_dim,
     const std::uint32_t num_units) {
+    LLK_ASSERT(
+        (tile_index < get_dest_max_tiles<DST_SYNC_MODE, DST_ACCUM_MODE, DstTileShape::Tile32x32>()),
+        "Tile index exceeds maximum allowed for the given tile shape and accumulation mode.");
+
     const std::uint8_t output_id = get_output_id(output);
-
     const std::uint32_t pack_tile_addr = get_output_tile_address<true, false>(output_id, output_tile_index);
-
     _llk_pack_fast_tilize_block_(tile_index, pack_tile_addr, unit_dim, num_units);
 }
 
