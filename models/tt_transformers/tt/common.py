6fed9817c8d669775e9e47214c510f6f88203612
Nikola Ostojic <nostojic@tenstorrent.com>
2026-02-10 19:22:03 +0000

    fixed

--- models/tt_transformers/tt/common.py
+++ models/tt_transformers/tt/common.py
@@ -16,10 +16,11 @@ from pydantic import AliasChoices, BaseModel, Field
 
 import ttnn
 
-        
+
 class Mode(Enum):
     DECODE = "decode"
     PREFILL = "prefill"
+
 
 class HostEmbedding(torch.nn.Module):
     def __init__(self, model_args):
